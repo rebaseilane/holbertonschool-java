75e59e9401ef7043c56201b0b923d183bbe4ce16
Rebaone Seilane <rebaseilane@gmail.com>
2026-03-26 08:46:05 +0000

    Task 1.2.3

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -1,7 +1,6 @@
 public class PrintStringVariable {
     public static void main(String[] args) {
         String text = "Holberton School";
-        System.out.println(text);
-        System.out.println(text); System.out.println(text.substring(0, 9));
+        System.out.println(text); System.out.println(text); System.out.println(text.substring(0, 9));
     }
 }

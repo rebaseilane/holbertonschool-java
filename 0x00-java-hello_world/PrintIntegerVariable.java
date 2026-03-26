76154d17a48631f52694051e841e21ff3e42b505
Rebaone Seilane <rebaseilane@gmail.com>
2026-03-26 08:41:34 +0000

    Task 1.2.3.4.5

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,6 @@
+public class PrintIntegerVariable {
+    public static void main(String[] args) {
+        int number = 1600;
+        System.out.println("Pennsylvania Avenue, " + number);
+    }
+}
---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String text1 = "Holberton";
+        String text2 = "School";
+        String result = "Welcome to " + text1 + " " + text2 + " !";
+        System.out.println(result);
+    }
+}

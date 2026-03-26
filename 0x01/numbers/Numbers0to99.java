ed3423d0d7f401471c0ea49ba88434b8515f9a08
Rebaone Seilane <rebaseilane@gmail.com>
2026-03-26 09:04:02 +0000

    Task 0

---
+++ 0x01/numbers/Numbers0to99.java
@@ -0,0 +1,11 @@
+public class Numbers0to99 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i == 99)
+                System.out.print(i);
+            else
+                System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}

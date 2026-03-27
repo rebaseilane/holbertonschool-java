7bd9fc2644d525142e0fdc1d4b42477e111de447
Rebaone Seilane <rebaseilane@gmail.com>
2026-03-27 10:50:07 +0000

    Task 0

--- 0x03/salary/Program.java
+++ 0x03/salary/Program.java
@@ -1,5 +1,11 @@
+import java.util.Locale;
+
 public class Program {
         public static void main(String[] args) {
+
+		Locale.setDefault(Locale.FRANCE);
+
+
             Department engineeringDepartment = new Department(1000, 1100);
             Department documentationDepartment = new Department(1000, 800);
 

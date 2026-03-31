30963320bdb5094c2a7dc9424bba093f1f5b8fb5
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-03-31 09:08:19 +0000

    Task 0,1.2

--- Lambda/markup/Program.java
+++ Lambda/markup/Program.java
@@ -3,16 +3,21 @@ public class Program {
 
         Product product1 = new Product(129.99, "Mouse Gamer");
 
-        System.out.printf("%s - %.2f - %.2f\n",
+        String price1 = String.format("%.2f", product1.getPrice()).replace('.', ',');
+        String markup1 = String.format("%.2f", product1.priceWithMarkup.get()).replace('.', ',');
+
+        System.out.printf("%s - %s - %s\n",
                 product1.getName(),
-                product1.getPrice(),
-                product1.priceWithMarkup.get());
+                price1,
+                markup1);
 
         product1.updateMarkup.accept(15d);
 
-        System.out.printf("%s - %.2f - %.2f\n",
+        String markup2 = String.format("%.2f", product1.priceWithMarkup.get()).replace('.', ',');
+
+        System.out.printf("%s - %s - %s\n",
                 product1.getName(),
-                product1.getPrice(),
-                product1.priceWithMarkup.get());
+                price1,
+                markup2);
     }
 }
--- Lambda/net/Program.java
+++ Lambda/net/Program.java
@@ -7,8 +7,13 @@ public class Program {
         double netSalaryPlus50 = Payroll.calculateNet(employee1, x -> x + 50);
         double netSalaryPlus10percent = Payroll.calculateNet(employee1, x -> x * 1.1);
 
-        System.out.printf("%.2f - %.2f\n", employee1.getSalary(), netSalary90percent);
-        System.out.printf("%.2f - %.2f\n", employee1.getSalary(), netSalaryPlus50);
-        System.out.printf("%.2f - %.2f\n", employee1.getSalary(), netSalaryPlus10percent);
+        String salary = String.format("%.2f", employee1.getSalary()).replace('.', ',');
+        String net1 = String.format("%.2f", netSalary90percent).replace('.', ',');
+        String net2 = String.format("%.2f", netSalaryPlus50).replace('.', ',');
+        String net3 = String.format("%.2f", netSalaryPlus10percent).replace('.', ',');
+
+        System.out.printf("%s - %s\n", salary, net1);
+        System.out.printf("%s - %s\n", salary, net2);
+        System.out.printf("%s - %s\n", salary, net3);
     }
 }
